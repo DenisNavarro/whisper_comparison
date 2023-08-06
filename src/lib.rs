af58c813367d9868e8a28d3f7419147dc20e7f18
Denis Navarro <denis.navarro@mailfence.com>
2023-08-06 22:40:10 +0200

    :bulb: Edit two comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![forbid(unsafe_code)]
-//#![warn(clippy::all, clippy::pedantic, clippy::nursery)] // TODO: add this line
+//#![warn(clippy::pedantic, clippy::nursery)] // TODO: add this line
 #![allow(clippy::all, clippy::nursery, clippy::pedantic)] // TODO: remove this line
 
 pub mod whisper_burn;
